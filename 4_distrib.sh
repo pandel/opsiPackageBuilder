52161a9cd2fd3729c56df8a2b36afc29c58af37b
Holger Pandel <holger.pandel@googlemail.com>
2015-06-24 15:23:34 +0200

    Debreate Anpassungen

--- 4_distrib.sh
+++ 4_distrib.sh
@@ -1,3 +1,6 @@
 pyinstaller --noconfirm  pyinstaller-linux.spec
 cp dist/helpviewer/helpviewer dist/opsipackagebuilder
+chmod -x dist/opsipackagebuilder/*.so*
+chmod 777 dist/opsipackagebuilder/help
+chmod 666 dist/opsipackagebuilder/help/opsipackagebuilder.qhc
 rm -R -f dist/helpviewer
